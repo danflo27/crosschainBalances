ca53f6a49dc54495af247ad4fd5b7fbbd39e38a5
Daniel Florentino <florentinodc@outlook.com>
2023-06-20 08:45:07 -0400

    Update getUsers.js

--- scripts/getUsers.js
+++ scripts/getUsers.js
@@ -6,23 +6,22 @@ const { ethers } = require("hardhat")
 const { abi, bytecode } = require("usingtellor/artifacts/contracts/TellorPlayground.sol/TellorPlayground.json")
 const h = require("usingtellor/test/helpers/helpers.js")
 const assert = require('chai').assert
-const MerkleTreeJS = require("../src/MerkleTree")
-const MerkleTree = new MerkleTreeJS(Web3)
-const Snapshot2 = require("../src/Snapshot2")
+const Weights = require("../src/Weights")
 require("dotenv").config();
 //npx hardhat run scripts/getUsers.js --network mainnet
 
-const autoPayAddress = "0x9BE9B0CFA89Ea800556C6efbA67b455D336db1D0"
+const autopayAddress = "0x9BE9B0CFA89Ea800556C6efbA67b455D336db1D0" // polygon, mainnet, 
 
 async function getUserList(node2) {
     const newestBlock = await web3.eth.getBlockNumber()
-    Snap = new Snapshot2(autoPayAddress, newestBlock, web3, node2)
+    console.log(newestBlock);
+    Snap = new Weights(autopayAddress, newestBlock, web3, node2)
     let list = await Snap.getUsers(newestBlock)
     console.log("blockNumber", newestBlock);
     console.log("user stats", list);
 }
 
-
+// change NODE_URL_network
 getUserList(process.env.NODE_URL_MATIC)
     .then(() => process.exit(0))
     .catch(error => {
