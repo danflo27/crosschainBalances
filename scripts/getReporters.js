7ff5b55d9212e7e67e0d3394c33526c47c6fa112
Daniel Florentino <florentinodc@outlook.com>
2023-06-20 08:41:44 -0400

    Update getReporters.js

--- scripts/getReporters.js
+++ scripts/getReporters.js
@@ -8,20 +8,20 @@ const h = require("usingtellor/test/helpers/helpers.js")
 const assert = require('chai').assert
 const MerkleTreeJS = require("../src/MerkleTree")
 const MerkleTree = new MerkleTreeJS(Web3)
-const Snapshot2 = require("../src/Snapshot2")
+const Weights = require("../src/Weights")
 require("dotenv").config();
-//npx hardhat run scripts/getBalanceMap.js --network mainnet
+//npx hardhat run scripts/getReporters.js --network mainnet
 
 const oracleAddress = "0xD9157453E2668B2fc45b7A803D3FEF3642430cC0"
 
 async function getReporterList(node2) {
-    const newestBlock = await web3.eth.getBlockNumber()
-    Snap = new Snapshot2(oracleAddress, newestBlock, web3, node2)
-    let list = await Snap.getReporters(newestBlock)
+    const newestBlock = await web3.eth.getBlockNumber();
+    console.log(newestBlock);
+    Snap = new Weights(oracleAddress, newestBlock, web3, node2);
+    let list = await Snap.getReporters(newestBlock);
     console.log("blockNumber", newestBlock);
     console.log("reporter stats", list);
 }
-
 
 getReporterList(process.env.NODE_URL_MATIC)
     .then(() => process.exit(0))
