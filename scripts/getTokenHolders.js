a07c4d49e8c51898e096ed9220f7ab3702761e95
Daniel Florentino <florentinodc@outlook.com>
2023-06-20 08:54:35 -0400

    add all network addressess

--- scripts/getTokenHolders.js
+++ scripts/getTokenHolders.js
@@ -10,11 +10,14 @@ const Weights = require("../src/Weights")
 require("dotenv").config();
 //npx hardhat run scripts/getBalanceMap.js --network mainnet
 
+//const tokenAddress = "0xd58D345Fd9c82262E087d2D0607624B410D88242" // arbitrum
+//const tokenAddress = "0xAAd66432d27737ecf6ED183160Adc5eF36aB99f2" //gnosis
 //const tokenAddress = "0xe3322702bedaaed36cddab233360b939775ae5f1" // polygon
 const tokenAddress = "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0" // mainnet
+//const tokenAddress = "0xaf8cA653Fa2772d58f4368B0a71980e9E3cEB888" // optimism
 
 
-async function getPowerMap(node2) {
+async function getHolders(node2) {
     const newestBlock = await web3.eth.getBlockNumber()
     console.log(newestBlock)
     Snap = new Weights(tokenAddress, newestBlock, web3, node2)
@@ -24,7 +27,7 @@ async function getPowerMap(node2) {
 }
 
 
-getPowerMap(process.env.NODE_URL)
+getHolders(process.env.NODE_URL)
     .then(() => process.exit(0))
     .catch(error => {
         console.error(error);
