957aa48cd43c3d7882c2b63ab5fdc427cf93208d
Daniel Florentino <florentinodc@outlook.com>
2023-07-11 09:35:55 -0400

    Update getAllVotingWeights.js

--- scripts/getAllVotingWeights.js
+++ scripts/getAllVotingWeights.js
@@ -8,22 +8,32 @@ const h = require("usingtellor/test/helpers/helpers.js")
 const assert = require('chai').assert
 const MerkleTreeJS = require("../src/MerkleTree")
 const MerkleTree = new MerkleTreeJS(Web3)
-const Weights = require("../src/Weights")
-import { getReporterList } from "./getReporters.js"
-import { getTokenHolderList } from "./getTokenHolders.js"
-import { getUserList } from "./getUsers.js"
+const Snapshot2 = require("../src/Weights")
+const getReporters = require("./getReporters.js")
+const getUsers = require("./getUsers.js")
+const getTokenHolders = require("./getTokenHolders.js")
 require("dotenv").config();
 
-const autopayAddress = "0x9BE9B0CFA89Ea800556C6efbA67b455D336db1D0";
+const autoPayAddress = "0x9BE9B0CFA89Ea800556C6efbA67b455D336db1D0";
 const oracleAddress = "0xD9157453E2668B2fc45b7A803D3FEF3642430cC0";
 const tokenAddress = "0xe3322702bedaaed36cddab233360b939775ae5f1" // polygon
 //const tokenAddress = "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0" // mainnet
 
+// doesnt work
+async function getAllVotes(node2) {
+    const newestBlock = await web3.eth.getBlockNumber()
+    let Reports = new Snapshot2(oracleAddress, newestBlock, web3, node2);
+    let Holders = new Snapshot2(tokenAddress, newestBlock, web3, node2);
+    let Users = new Snapshot2(autoPayAddress, newestBlock, web3, node2);
+    let reporterList = await Reports.getAllWeights(newestBlock);
+    let holderList = await Holders.getAllWeights(newestBlock);
+    let userList = await Users.getAllWeights(newestBlock);
+    console.log("blockNumber", newestBlock);
+    console.log("voting stats", holderList, userList, reporterList);
+}
 
 
-
-
-getAllVotes(process.env.NODE_URL_MATIC)
+getAllVotes(process.env.NODE_URL_OPTIMISM)
     .then(() => process.exit(0))
     .catch(error => {
         console.error(error);
